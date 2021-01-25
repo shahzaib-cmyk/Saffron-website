bdc849818103a7a7b60aa44f7c9ee2947fa9d727
shahzaib-cmyk <shahzaibkh198@gamil.com>
2021-01-25 16:00:20 +0500

    made changes and addedd animations

--- js/main.js
+++ js/main.js
@@ -6,10 +6,20 @@ const navList=document.querySelector(".nav-list");
 const navLinks=document.querySelectorAll(".nav-links");
 const burgerMenu=document.querySelector(".burger-menu");
 const burgerLines=document.querySelectorAll(".burger-line");
-
-
-
-
+const content=document.querySelector(".content");
+let contentTween=gsap.from(".content",{
+  x:-1100,
+  opacity:0,
+  scale:0.5,
+  duration:0.7,
+  onComplete:expandBtn
+})
+const temp=document.querySelector(".temp");
+let tempTween=gsap.from(".temp",{
+  xPercent:100,
+  yPercent:-100,
+  scale:0.5
+})
 //          EVENT LISTENERS
 logo.addEventListener("click",logoRoll)
 burgerMenu.addEventListener("click",logoRoll);
@@ -25,11 +35,25 @@ function logoRoll() {
     link.classList.toggle("nav-link-expand");
     link.style.animationDelay=`${index/6+0.8}s`;
     /*link.style.animation=`nav-link-expander 2s ease-in ${index/6+0.6}s`*/
-    console.log(`${index/6}s`);
   })
   burgerLines[1].classList.toggle("burger-line-even-exp")
   burgerLines[2].classList.toggle("burger-line-2-exp")
-    burgerLines[0].classList.toggle("burger-line-3-exp")
+    burgerLines[0].classList.toggle("burger-line-3-exp");
 
 
+}
+function expandBtn(){
+  let btnTween=gsap.fromTo(".btns",{
+    opacity:0,
+  duration:0.5,
+    scale:0.2,
+    ease:"elastic"
+  },{
+    opacity:1,
+    stagger:0.2,
+
+    scale:1,
+    
+  })
+  
 }
